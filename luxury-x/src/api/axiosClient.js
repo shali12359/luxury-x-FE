145de1ae5ec04301a8eccf198a8b7588484a1521
Prabath Shalitha <58390521+shali12359@users.noreply.github.com>
2026-01-02 23:17:22 +0530

    Update axiosClient.js

--- luxury-x/src/api/axiosClient.js
+++ luxury-x/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://3.110.44.15:8080/api",
+    baseURL: "http://3.109.212.112:8080/api",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
