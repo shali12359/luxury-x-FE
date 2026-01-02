1efb57bd0d8525d898e57a46d0bfef6f987ccb67
Prabath Shalitha <58390521+shali12359@users.noreply.github.com>
2026-01-02 23:48:07 +0530

    Update axiosClient.js ip

--- luxury-x/src/api/axiosClient.js
+++ luxury-x/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://3.109.212.112:8080/api",
+    baseURL: "http://13.200.242.14:8080/api",
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
