d61a6580822bd2ae4021ae8f2f4ee33d3f8fd792
Prabath Shalitha <58390521+shali12359@users.noreply.github.com>
2026-01-03 01:34:48 +0530

    Update CategoryApi.js

--- luxury-x/src/api/CategoryApi.js
+++ luxury-x/src/api/CategoryApi.js
@@ -3,7 +3,7 @@ import axiosClient from "./axiosClient";
 export const categoryApi = {
     create: (data) => axiosClient.post(`/category`, data),
     get: (id) => axiosClient.get(`/category/${id}`),
-    getAll: () => axiosClient.get(`category`),
+    getAll: () => axiosClient.get(`/category`),
     update: (id, data) => axiosClient.put(`/category/${id}`, data),
     delete: (id) => axiosClient.delete(`/category/${id}`)
 }
