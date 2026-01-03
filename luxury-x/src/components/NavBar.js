873e4418cd7ebeaefcedb288fe6a4093b4a65ebf
Prabath Shalitha <58390521+shali12359@users.noreply.github.com>
2026-01-03 10:05:22 +0530

    Update NavBar.js

--- luxury-x/src/components/NavBar.js
+++ luxury-x/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 
 function NavigationBar() {
   return (
@@ -8,7 +9,7 @@ function NavigationBar() {
         <Container>
           <Navbar.Brand href="/">Luxury X</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/categories">Categories</Nav.Link>
+            <Nav.Link><Link to={`/categories`} style={{ textDecoration: 'none', color: 'inherit' }}>Categories</Link></Nav.Link>
           </Nav>
         </Container>
       </Navbar>
